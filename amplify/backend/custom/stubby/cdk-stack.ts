4ee6645d095e7ce0b4b1f2bff4d163faf2af3329
Joseph Lawson <joe.lawson@dexcom.com>
2023-05-25 20:00:13 -0400

    add StubbyStack

---
+++ amplify/backend/custom/stubby/cdk-stack.ts
@@ -0,0 +1,18 @@
+import * as cdk from 'aws-cdk-lib';
+import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper';
+import { Construct } from 'constructs';
+import {StubbyStack} from "./stubby-stack";
+import {Fn} from "aws-cdk-lib";
+
+export class cdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
+    super(scope, id, props);
+    /* Do not remove - Amplify CLI automatically injects the current deployment environment in this input parameter */
+    new cdk.CfnParameter(this, 'env', {
+      type: 'String',
+      description: 'Current Amplify CLI env name',
+    });
+
+    new StubbyStack(scope, 'NestedStubbyStack', {env: Fn.ref('env')});
+  }
+}
---
+++ amplify/backend/custom/stubby/stubby-stack.ts
@@ -0,0 +1,43 @@
+import {NestedStack, NestedStackProps, RemovalPolicy} from "aws-cdk-lib";
+import {Construct} from "constructs";
+import {AttributeType, StreamViewType, Table} from "aws-cdk-lib/aws-dynamodb";
+import {Function, InlineCode, Runtime, StartingPosition} from "aws-cdk-lib/aws-lambda";
+import {Topic} from "aws-cdk-lib/aws-sns";
+import {DynamoEventSource, SnsDlq} from "aws-cdk-lib/aws-lambda-event-sources";
+
+export interface StubbyProps extends NestedStackProps{
+    env: string
+}
+export class StubbyStack extends NestedStack {
+    constructor(scope: Construct, id: string, props?: StubbyProps) {
+        super(scope, id, props);
+
+        // Dynamo DB Tables
+        const customTable = new Table(this, 'customTable', {
+            partitionKey: { name: 'id', type: AttributeType.STRING},
+            timeToLiveAttribute: 'expirationDate',
+            tableName: `custom-table-${props.env}`,
+            stream: StreamViewType.OLD_IMAGE,
+            removalPolicy: RemovalPolicy.DESTROY,
+            pointInTimeRecovery: true,
+        });
+
+        const reactToTableLambda = new Function(this, 'reactiveLambda', {
+            handler: "index.handler",
+            code: new InlineCode('return'),
+            runtime: Runtime.NODEJS_16_X
+        });
+
+        // SNS topic for DLQ
+        const snsTopic = new Topic(this, 'reactiveLambdaFail', {
+            displayName: 'Reactive Lambda Fail',
+        });
+
+        // lambda event source
+        reactToTableLambda.addEventSource(new DynamoEventSource(customTable, {
+            startingPosition: StartingPosition.LATEST,
+            retryAttempts: 3,
+            onFailure: new SnsDlq(snsTopic),
+        }));
+    }
+}
