2f78863fa714bf2dd85eafd340817565dd491955
Joseph Lawson <joe.lawson@dexcom.com>
2023-05-26 15:45:45 -0400

    ignore appId if REMOTE_BACKEND_EXISTS is set
    it's an amplify build so let amplify init do its thing

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,8 +3,12 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import {AugmentedAmplifyExportedBackend} from "../lib/AugmentedAmplifyExportedBackend";
 import {EXPORT_PATH} from "../lib/util"; // To resolve the path to your exported Amplify backend assets
+let appId = process.env.AWS_APP_ID || undefined;
+if(process.env.REMOTE_BACKEND_EXISTS != null) {
+    // this is a Amplify build so avoid messing with the backend
+    appId = undefined;
+}
 
-const appId = process.env.AWS_APP_ID || undefined;
 const env = process.env.USER_BRANCH || process.env.AWS_BRANCH || "dev"; // Specify your Amplify environment
 const app = new cdk.App();
 new AugmentedAmplifyExportedBackend(app, "exported", {
